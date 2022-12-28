b103db5486887146763d7d79715c3373b523bdc4
The Verse <floatbeta@gmail.com>
2022-12-28 05:35:36 -0600

    Update ipn.lua

--- SmartContracts/ipn.lua
+++ SmartContracts/ipn.lua
@@ -8,27 +8,27 @@ local NOTIFICATION_ID_PATTERN = '^' .. NOTIFICATION_ID_PREFIX .. '[a-z0-9]+$'
 
 -- define the contract functions
 
--- function to send a payment notification
-function sendPaymentNotification(to, payment)
+-- function to send a transaction notification
+function sendTransactionNotification(to, transaction)
   -- check the input parameters
   assert(to, 'missing to address')
-  assert(payment, 'missing payment details')
+  assert(transaction, 'missing transaction details')
 
   -- generate a unique notification id
   local notificationId = NOTIFICATION_ID_PREFIX .. moltenchain.random()
 
   -- store the notification in the contract state
   moltenchain.setState(notificationId, {
-    type = 'payment',
-    data = payment,
+    type = 'transaction',
+    data = transaction,
   })
 
   -- send the notification to the recipient
   moltenchain.send(to, notificationId)
 end
 
--- function to retrieve a payment notification
-function getPaymentNotification(notificationId)
+-- function to retrieve a transaction notification
+function getTransactionNotification(notificationId)
   -- check the input parameter
   assert(notificationId, 'missing notification id')
   assert(string.match(notificationId, NOTIFICATION_ID_PATTERN), 'invalid notification id')
