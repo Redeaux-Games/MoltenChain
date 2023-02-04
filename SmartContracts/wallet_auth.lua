a8de16a53f2616ac09db0270e04da1be5058a515
The Verse <floatbeta@gmail.com>
2023-02-03 19:37:12 -0600

    Update wallet_auth.lua

--- SmartContracts/wallet_auth.lua
+++ SmartContracts/wallet_auth.lua
@@ -1,22 +1,32 @@
--- constants
-local USER_ADDRESS_LENGTH = 20  -- length of an Ethereum address in bytes
+-- Transaction fee smart contract for MoltenChain
 
--- variables
-local authorizedUsers = {}  -- set of authorized user addresses
+-- Constants
+local FEE_AMOUNT = 0.00001 -- 0.00001 MAGMA
+local DEV_WALLET = "dev.magma"
 
--- functions
-function addAuthorizedUser(userAddress)
-  -- check that the user address is of the correct length
-  assert(#userAddress == USER_ADDRESS_LENGTH, "Invalid user address")
+-- Function to charge transaction fee
+function chargeFee(transaction)
+    -- Get the sender's wallet address
+    local sender = transaction.from
 
-  -- add the user to the set of authorized users
-  authorizedUsers[userAddress] = true
-end
+    -- Check if the sender has enough balance
+    if sender.balance < FEE_AMOUNT then
+        return false, "Insufficient funds"
+    end
 
-function isAuthorized(userAddress)
-  -- check that the user address is of the correct length
-  assert(#userAddress == USER_ADDRESS_LENGTH, "Invalid user address")
+    -- Deduct the fee from the sender's balance
+    sender.balance = sender.balance - FEE_AMOUNT
 
-  -- return whether the user is authorized
-  return authorizedUsers[userAddress] == true
+    -- Get the developer's wallet
+    local devWallet = getWallet(DEV_WALLET)
+
+    -- Add the fee to the developer's wallet
+    devWallet.balance = devWallet.balance + FEE_AMOUNT
+
+    -- Update the wallets
+    updateWallet(sender)
+    updateWallet(devWallet)
+
+    -- Return true to indicate that the transaction was successful
+    return true
 end
